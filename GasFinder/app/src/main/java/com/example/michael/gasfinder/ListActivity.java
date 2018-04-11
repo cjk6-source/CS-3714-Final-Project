3f7de9841c6d0dcf19bb1bd64b45a8de921d3958
Mint3kool <stay9@vt.edu>
2018-04-11 17:14:58 -0400

    Added in the necessary API java files for me, started changing my xml.

--- GasFinder/app/src/main/java/com/example/michael/gasfinder/ListActivity.java
+++ GasFinder/app/src/main/java/com/example/michael/gasfinder/ListActivity.java
@@ -1,13 +1,31 @@
 package com.example.michael.gasfinder;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ListActivity extends AppCompatActivity {
 
+
+    Location myLocation;
+    GasAPI gasAPI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        myLocation = new Location(LocationManager.GPS_PROVIDER);
+
+        //TODO(TAY): Will need to get latitude/longitude from some input class
+        myLocation.setLatitude(37.228260);
+        myLocation.setLongitude(-80.428548);
+
+        gasAPI = new GasAPI(this);
+    }
+
+    public void getLocations(Location l) {
+        gasAPI.getNearbyStations(l, 100, "Unleaded", "Distance");
     }
 }
