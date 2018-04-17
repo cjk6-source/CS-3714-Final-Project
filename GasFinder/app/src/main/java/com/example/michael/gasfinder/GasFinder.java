1657acccf34189023cd3cc61837aa2682cae9353
Drauren <drauren@vt.edu>
2018-04-17 00:24:18 -0400

    added request for GPS Course and Fine Access in menu activity, also added permissions in manifest.xml

--- GasFinder/app/src/main/java/com/example/michael/gasfinder/GasFinder.java
+++ GasFinder/app/src/main/java/com/example/michael/gasfinder/GasFinder.java
@@ -1,6 +1,10 @@
 package com.example.michael.gasfinder;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,9 +21,9 @@ public class GasFinder extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gas_finder);
 
-        toHistory = (Button) findViewById(R.id.toHistory);
-        toLogger = (Button) findViewById(R.id.toLogger);
-        toList = (Button) findViewById(R.id.toList);
+        toHistory = findViewById(R.id.toHistory);
+        toLogger = findViewById(R.id.toLogger);
+        toList = findViewById(R.id.toList);
     }
 
     public void onClickList(View view)
@@ -38,5 +42,24 @@ public class GasFinder extends AppCompatActivity {
     {
         Intent intent = new Intent(this, HistoryActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        }, 0);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            //gpsManager.register();
+        }
     }
 }
