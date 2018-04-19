f6f56b2460561fe949f01d7161ee6d1450725d09
Puvanai Avakul <nicky@MacBook-Pro-2.local>
2018-04-19 13:10:33 -0400

    Updates for Gas History -updated DBOpenHelper column names and added _id column -updated HistoryActivity class to include listview and show items in the database -updated the the xml files with more intuitive ids -updated the custom layout to include more fields -added DatabaseManager class

--- GasFinder/app/src/main/java/com/example/michael/gasfinder/HistoryActivity.java
+++ GasFinder/app/src/main/java/com/example/michael/gasfinder/HistoryActivity.java
@@ -1,40 +1,60 @@
 package com.example.michael.gasfinder;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-public class HistoryActivity extends AppCompatActivity implements View.OnClickListener
-{
+public class HistoryActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button back;
     Button clear_all;
     TextView total;
     ListView history_list;
 
-    /* Adapter for list view */
-    /* Database manager */
+    DatabaseManager databaseManager;
+    SimpleCursorAdapter adapter;
 
+    static final String[] fromColumns = {
+            DBOpenHelper.COLUMN_STATION_NAME,
+            DBOpenHelper.COLUMN_TOTAL_EXPENSE,
+            DBOpenHelper.COLUMN_ADDRESS,
+            DBOpenHelper.COLUMN_FUEL_TYPE,
+            DBOpenHelper.COLUMN_DATE,
+            DBOpenHelper.COLUMN_PRICE_PER_GALLON
+    };
+
+    static final int[] toViews = {
+            R.id.station_name,
+            R.id.amount_spent,
+            R.id.address,
+            R.id.type,
+            R.id.date,
+            R.id.ppg
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
         back = findViewById(R.id.back);
         clear_all = findViewById(R.id.clear_all);
-        total = findViewById(R.id.total_value);
+        total = findViewById(R.id.total_sum);
         history_list = findViewById(R.id.history_list);
 
         back.setOnClickListener(this);
         clear_all.setOnClickListener(this);
 
-        /* SimpleCursorAdapter */
-        /* Database manager */
+        databaseManager = new DatabaseManager(this);
+        databaseManager.open();
+        adapter = new SimpleCursorAdapter(this, R.layout.custom_history_row,
+                databaseManager.getAllRecords(), fromColumns, toViews, 0);
+        history_list.setAdapter(adapter);
     }
 
     @Override
@@ -45,7 +65,15 @@ public class HistoryActivity extends AppCompatActivity implements View.OnClickLi
                 break;
 
             case R.id.clear_all:
-                /* Clear the list view */
+                /*databaseManager.insertHistoryInfo("Test Station",
+                        "841 Claytor Sq",
+                        "05/10/1996",
+                        35.5,
+                        "Premium",
+                        3.4);*/
+                databaseManager.deleteAll();
+                adapter.changeCursor(databaseManager.getAllRecords());
+                adapter.notifyDataSetChanged();
                 break;
         }
     }
@@ -53,6 +81,6 @@ public class HistoryActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        /* Close the database */
+        databaseManager.close();
     }
 }
