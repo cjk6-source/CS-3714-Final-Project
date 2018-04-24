b200ab01642a8903ce6e12cefe90e99a3b590adf
Drauren <drauren@vt.edu>
2018-04-23 23:17:31 -0400

    MapActivity now correctly display markers, code and function to handle onmarkerclick to details screen is there, but crashes due to resourcenotfound exception when called

--- GasFinder/app/src/main/java/com/example/michael/gasfinder/MapActivity.java
+++ GasFinder/app/src/main/java/com/example/michael/gasfinder/MapActivity.java
@@ -4,17 +4,19 @@ import android.content.Intent;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener
 {
     private GoogleMap gMap;
     private ArrayList<GasStation> nearbyStations;
@@ -40,11 +42,13 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
+        gMap.setOnMarkerClickListener(this);
         if(gMap!=null)
         {
             markStationLocations();
@@ -53,7 +57,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback
 
     public void markStationLocations()
     {
-        if(nearbyStations.size()>0)
+        if(nearbyStations != null)
         {
             for(int i=0;i<nearbyStations.size();i++)
             {
@@ -61,7 +65,28 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback
                 gMap.addMarker(new MarkerOptions().position(tempStation)
                         .title(nearbyStations.get(i).getStationName()));
             }
-            gMap.moveCamera(CameraUpdateFactory.newLatLng(curUserLatlng));
+            float zoomLevel = 16.0f;
+            gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curUserLatlng,zoomLevel));
         }
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        Log.d("Marker position lat: ", String.valueOf(marker.getPosition().latitude));
+        Log.d("Marker position long: ", String.valueOf(marker.getPosition().longitude));
+        if(nearbyStations !=  null)
+        {
+            for (int i = 0; i < nearbyStations.size(); i++)
+            {
+                if (nearbyStations.get(i).getLatitude() == marker.getPosition().latitude && nearbyStations.get(i).getLongitude() == marker.getPosition().longitude)
+                {
+                    //Log.d("Station Name: ", nearbyStations.get(i).getStationName());
+                    Intent intent = new Intent(this, DetailsScreen.class);
+                    intent.putExtra("GAS STATION OBJECT", nearbyStations.get(i));
+                    startActivity(intent);
+                }
+            }
+        }
+        return true;
     }
 }
